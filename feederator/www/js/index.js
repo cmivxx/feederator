eada8cf6f598e39a176d2c7924901e250237da7d
Chris Miller <chris@intellagentbenefits.com>
2015-11-30 18:20:47 -0600

    added in service, not quite working.

--- feederator/www/js/index.js
+++ feederator/www/js/index.js
@@ -39,28 +39,38 @@ angular.module('ionicApp', ['ionic'])
   $urlRouterProvider.otherwise("/menu/home");
 })
 
-.controller('HomeCtrl', function($scope, $http, $ionicSideMenuDelegate) {
+.controller('HomeCtrl', function($scope, $http, $ionicSideMenuDelegate, dataLoad) {
 	console.log("in home controller");
-	var feeds = {};
-	var feed_id = 0;
-	//$http.get('http://oapi.cmivxx.com/v2/feeds').success(function(data) {
-	$http.get('feedList.json').success(function(data) {
-		feeds.content = data;
-		$scope.ifeeds = data;
-	}).error(function(data) {
-		console.log("houston, we have a problem...");
-	}).then(function(data) {
-		angular.forEach(feeds.content.feeds, function(feed_inf, key) {
-			//$http.get("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "num": "10", "q": feed_inf.rss } }).success(function(fdata) {
-			$http.get("sampleFeed.json", { params: { "v": "1.0", "limit": "10", "q": feed_inf.rss } }).success(function(fdata) {
-				$scope.ifeeds.feeds[key].entries = fdata;
-      		}).error(function(fdata) {
-				console.log("houston, we have a different problem...");
-      		}).then(function(fdata) {
-				feed_id++;
-      		})
-    		})
-  	})
+	//var feeds = {};
+	//var feed_id = 0;
+	// $http.get('http://oapi.cmivxx.com/v2/feeds').success(function(data) {
+	// //$http.get('feedList.json').success(function(data) {
+	// 	feeds.content = data;
+	// 	$scope.ifeeds = data;
+	// }).error(function(data) {
+	// 	console.log("houston, we have a problem...");
+	// }).then(function(data) {
+	// 	angular.forEach(feeds.content.feeds, function(feed_inf, key) {
+	// 		$http.get("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "num": "10", "q": feed_inf.rss } }).success(function(fdata) {
+	// 		//$http.get("sampleFeed.json", { params: { "v": "1.0", "limit": "10", "q": feed_inf.rss } }).success(function(fdata) {
+	// 			$scope.ifeeds.feeds[key].entries = fdata;
+ 	//		}).error(function(fdata) {
+	// 			console.log("houston, we have a different problem...");
+ 	//		}).then(function(fdata) {
+	// 			feed_id++;
+ 	//		})
+ 	//	})
+ 	//})
+	$scope.dataLoad = dataLoad.dataLoad("all");
+
+	if ($scope.dataLoad === 1) {
+		console.log("data loaded:");
+		console.log(localStorage.getItem("feedData"));
+		$scope.ifeeds = localStorage.getItem("feedData");
+		console.log($scope.ifeeds);
+	} else {
+		console.log("Something broke...");
+	}
 })
 
 .controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
@@ -110,4 +120,39 @@ angular.module('ionicApp', ['ionic'])
     }
   };
 
+})
+
+.service('dataLoad', function($http) {
+  // Fetch function
+	this.dataLoad = function getData() {
+		console.log("fetching data.");
+		var feeds = {};
+		var feed_id = 0;
+		//$http.get('http://oapi.cmivxx.com/v2/feeds').success(function(data) {
+		$http.get('feedList.json').success(function(data) {
+			feeds.content = data;
+			//$scope.ifeeds = data;
+			console.log("feed data:");
+			console.log(feeds.content);
+		}).error(function(data) {
+			console.log("houston, we have a problem...");
+		}).then(function(data) {
+			angular.forEach(feeds.content.feeds, function(feed_inf, key) {
+				//$http.get("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "num": "10", "q": feed_inf.rss } }).success(function(fdata) {
+				$http.get("sampleFeed.json", { params: { "v": "1.0", "limit": "10", "q": feed_inf.rss } }).success(function(fdata) {
+					//$scope.ifeeds.feeds[key].entries = fdata;
+					feeds.content.feeds[key].entries = fdata;
+	      		}).error(function(fdata) {
+					console.log("houston, we have a different problem...");
+	      		}).then(function(fdata) {
+					feed_id++;
+	      		})
+	    		})
+	  	}).then(function(data) {
+	  		localStorage.setItem("feedData", feeds.content.feeds);
+	  		console.log("saved data");
+	  		console.log(localStorage.getItem("feedData"));
+	  	})
+	  	return 1;
+	}
 });
